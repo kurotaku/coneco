b15f8b5bdda52a4c0192572adc6b83e3b5794ff9
kurotaku <kuroiwa.taku@gmail.com>
2019-06-19 12:20:06 +0200

    store image at local

--- config/initializers/carrieawave.rb
+++ config/initializers/carrieawave.rb
@@ -26,18 +26,18 @@
 # end
 
 # # sftp setting
-if Rails.env == 'production'
-  CarrierWave.configure do |config|
-    config.sftp_host = ENV['FTP_HOST']
-    config.sftp_user = ENV['FTP_USER']
-    config.sftp_folder = ENV['FTP_PATH']
-    config.sftp_url = ENV['FTP_URL']
-    config.sftp_options = {
-      :password => ENV['FTP_PASS'],
-      :port => 22
-    }
-  end
-end
+# if Rails.env == 'production'
+#   CarrierWave.configure do |config|
+#     config.sftp_host = ENV['FTP_HOST']
+#     config.sftp_user = ENV['FTP_USER']
+#     config.sftp_folder = ENV['FTP_PATH']
+#     config.sftp_url = ENV['FTP_URL']
+#     config.sftp_options = {
+#       :password => ENV['FTP_PASS'],
+#       :port => 22
+#     }
+#   end
+# end
 
 module CarrierWave
   module MiniMagick
